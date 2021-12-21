e5c87b7256612a711a66e2cbe9c4103724fa334f
dosora1 <dosora9@gmail.com>
2021-12-21 22:23:25 +0900

    Update temi_humidifier.ino

--- temi_humidifier/temi_humidifier.ino
+++ temi_humidifier/temi_humidifier.ino
@@ -1,22 +1,26 @@
-#define ON 1
-#define OFF 0
 
-bool humidifier = OFF;
+int humidifier = '0';
 int humidified = 5;
 int led = 6;
 int trig = 12;
 int echo = 13;
+int time_flag=0;
 
 void setup() {
   pinMode(humidified, OUTPUT);
   pinMode(led, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  if (humidity < 40 || humidifier == ON) {
+  if(Serial.available())
+    humidifier = Serial.read();
+      
+  if (humidifier == '1') {
     digitalWrite(humidified, HIGH);
     digitalWrite(led, HIGH);
-  } else {
+  }
+  else {
     digitalWrite(humidified, LOW);
     digitalWrite(led, LOW);
   }
