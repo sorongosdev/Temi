b6e5dbf6a9d3ee0794e7b12d720042dc8ec9c50c
dosora1 <dosora9@gmail.com>
2021-12-26 15:39:21 +0900

    Update temi_name.h

--- temi_arduino/temi_arduino/temi_name.h
+++ temi_arduino/temi_arduino/temi_name.h
@@ -8,8 +8,7 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LED, NEO_PIN, NEO_GRB + NEO_KHZ800
 PM2008_I2C pm2008;
 
 //Const
-const String knownKeys[] = {"8B:A8:0C:22","E7:FB:D4:10","C9:98:E3:4F"}; //White, Pink, ID
-//const String Name[] = {"DSR","YYR","YJY","CYS","LSH","BJC"};
+const String knownKeys[] = {"C9:98:E3:4F","79:3E:AB:E1","21:F4:22:50","AF:1C:96:60","59:54:23:D2","CF:9F:95:60"}; //White, Pink, ID
 
 String strID = ""; //RFID
 int rfid_id = 9, id_flag=9;
@@ -23,6 +22,6 @@ int locker,locker_flag;
 
 int pixel_delay = 100; //pixel
 int r,g,b;
-int pixel_color, prev_color='4';
+int pixel_color, prev_color='g';
 
 long unsigned Time,time_flag=0; //Time
